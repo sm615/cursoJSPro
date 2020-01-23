bda8a4c7178c8320915584c7f15943c1a50988b8
Santiago Mozo <smozo615@gmail.com>
2020-01-22 22:15:14 -0500

    Cómo llega un script al navegador

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,8 @@ class MediaPlayer
     constructor(config) {
         this.media = config.el
     }
-    play()
+    
+    togglePlay()
     {
         if (this.media.paused) {
             this.media.play()
@@ -18,4 +19,13 @@ class MediaPlayer
 
 const player = new MediaPlayer({ el: video });
 
-button.onclick = () => player.play();
+button.onclick = () => player.togglePlay();
+
+/* ----------------------Cómo llega un script al navegador ----------------------*/
+
+/* atributo async en los scripts externos permite que el DOM se siga cargando mientras hacemos la peticion del script externo
+por ejemplo: google analytics; una vez se resulva la peticion, se detiene la carga del DOM y se ejecuta el js para luego
+seguir con la carga del DOM */
+
+/* atributo defer: este atributo va a dejar la ejecucion del archivo js hasta el final, la peticion es asincrona 
+pero deja la ejecucion al final */
