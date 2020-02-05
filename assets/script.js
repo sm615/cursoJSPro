b18a658082fa7affb6b220114264d87538231dc0
Santiago Mozo <smozo615@gmail.com>
2020-02-04 21:57:52 -0500

    Los metodos call, apply y bind

--- assets/script.js
+++ assets/script.js
@@ -17,23 +17,59 @@ playButton.onclick = () => player.togglePlay()
 const soundButton = document.getElementById('sound_btn')
 soundButton.onclick = () => player.sound()
 
-// cuando una funcion incluye 'this' y la llamos directamente, el motor de js la interpreta dandola como referencia a window(ventana)
-//this se refiere al objeto que esta ejecutando un pedazo o parte del codigo actualmente
-// el modo estricto lo podemos activar con el string "use strict" en cualquier parte del texto
+// call applay y bind no ayudan a cambiar el contexto de this
 
-// en el siguiente ejemplo podemos ver como this hace referencia al objeto porque la funcion esta dentro del objeto
-const person = {
-    name: 'santiago',
-    saludar: function () {
-        console.log(`hola mi nombre es: ${this.name}`);
-    }
+
+function caminar(metros, direccion) {
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y hoy caminé: ${metros}mts en dirección ${direccion}`);
 }
-/*person.saludar() */
 
-// pero si sacamos esa funcion del objeto, accion (en este caso), no se esta llamando en el contexto del objeto,
-// por lo tanto name no existe ya que solo se encuentra de declarado dentro del objeto persona  
+const santiago = {
+    nombre: 'Santiago',
+    apellido: 'Mozo'
+}
 
-const accion = person.saludar;
-accion()
+// CAll
+// caminar.call(santiago, 400, 'norte') //el primer argumento que pasamos es el contexto de this, luego pasamos los argumentos de la funcion 
 
-//this dentro de objetos o "clases" se va a referir a la instancia de esa clase
+// APPLY 
+// es similar a call, solo que los argumentos se pasan de manera ligeramente distinta
+
+// caminar.apply(santiago, [40, 'norte']) // los parametros de la funcion se pasa como parte de un arreglo
+
+// hay ocasiones en que es mas facil utilizar apply, por ejemplo, si ya tenemos los valores como parte de un arreglo
+
+// const valores = [50, 'noreste']
+// caminar.apply(santiago, valores)
+
+/* recuerda
+    call - valores separados por Coma
+    apply - valores dentro de un Arreglo
+*/
+
+// Bind
+// bind a diferencia de call y apply no va a llamar a la funcion automaticamente, solo va a contruir una nueva funcion con el this ya integrado
+
+const daniel = {
+    nombre: 'Daniel',
+    apellido: 'Sanchez'
+}
+
+const danielCamina = caminar.bind(daniel) //aqui se esta declarando una nueva funcion en la cual se entrelaza caminar con el objeto daniel
+
+danielCamina(40, 'norte') // aqui estamos invocando esa nueva funcion
+
+// tambien podemos pasarle argumentos al bind
+
+const danielCamina2 = caminar.bind(daniel, 5000, 'sur')
+danielCamina2()
+
+// o incluso de manera parcial
+
+const danielCamina3 = caminar.bind(daniel, 300)
+danielCamina3('oeste')
+
+//cuando obtenemos elementos de html por su clase se crea un nodelist que es parecido a un array pero que no tiene alguno de sus metodos
+//en estos caso podemos usar call, apply o bind para cambiar el contexto del array
+
+// Array.prototype.forEach.call(__nodelist, funcion_ejecutar)
